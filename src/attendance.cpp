8c7eb0b7879debca3156e92775809d1294993dfa
black-OWL432 <chongchun0109@gmail.com>
2025-12-03 19:10:15 +0800

    Initial commit

---
+++ src/attendance.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************************
+ * Program: attendance.cpp
+ * Course: CCP6114 Programming Fundamentals
+ * Lecture Class: TC1L
+ * Tutorial Class: TT1L/TT3L
+ * Trimester: 2530
+ * 
+ * Member 1: 252UC254Q2	WILLIAM TAN		.....@student.mmu.edu.my			011-2746 8433
+ * Member 2: 252UC254RG	HOONG ZHEN YI	.....@student.mmu.edu.my			012-945 6377
+ * Member 3: 252UC254YW	LEE CHONG CHUN	lee.chong.chun1@student.mmu.edu.my	019-646 2399
+ * Member 4: 253UC2565E	SHAAN VINAAYAK	shaan.vinaayak@student.mmu.edu.my	016-930-2705
+ * 
+ * ******************************************************************************************
+ * 
+ * Task distribution
+ * Member 1:
+ * Member 2:
+ * Member 3:
+ * Member 4:
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
